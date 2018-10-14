426ef4ba20d8d2af4206b9bb62c6d93c600176b2
Astocker <Astocker@mail.bradley.edu>
2018-10-14 16:51:39 -0500

    Implemented player movement and animations.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    //private Vector3 offset;
+    private bool cameraMove;
+    private float distance;
+    private float speed;
+    private float y;
+    private float x;
+    private float z;
+    private Vector3 lastPosition;
+    private Quaternion lastRotation;
+
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        distance = 7.21f;
+        y = angles.y;
+        x = angles.x;
+        speed = 50.0f;
+
+        lastPosition = transform.position;
+        lastRotation = transform.rotation;
+    }
+
+    void LateUpdate()
+    {
+        if (!player.GetComponent<Player>().isPaused)
+        {
+            y += Input.GetAxis("Mouse X") * speed * distance * 0.02f;
+            x += Input.GetAxis("Mouse Y") * speed * distance * 0.02f;
+
+            var rotation = Quaternion.Euler(Mathf.Clamp(-x, 2, 65), y, 0);
+            var position = rotation * new Vector3(0.0f, 0.0f, -distance) + player.transform.position;
+            transform.rotation = rotation;
+            transform.position = position;
+
+            RaycastHit hit = new RaycastHit();
+            Ray wallRay = new Ray(player.transform.position, transform.position - player.transform.position);
+
+            if (Physics.Raycast(wallRay, out hit, distance))
+            {
+                if (!hit.collider.gameObject.CompareTag("Player"))
+                {
+                    transform.position = hit.point;
+                }
+            }
+
+            lastPosition = transform.position;
+            lastRotation = transform.rotation;
+        }
+        else
+        {
+            transform.position = lastPosition;
+            transform.rotation = lastRotation;
+        }
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,89 +6,189 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     public float maxHealth;
+    public float speed;
+    public float turnSpeed;
+    public float jumpSpeed;
     public float power;
     public Vector3 respawn;
-    //public Text countText;
-    //public float collectGoal;
-    //public GameObject WinCanvas;
-    //public Slider healthBar;
+    public Quaternion respawnRotation;
+    public Slider healthBar;
     public bool isPaused;
 
+
     private float currHealth;
-    //private bool isGrounded;
-    //private int count;
-    //private AudioSource audioSource;
-    //private float floorDist;
-    //private Transform weaponTransform;
-    //private Rigidbody rigid;
+    private bool isGrounded;
+    private float floorDist;
+    private Transform weaponTransform;
+    private Rigidbody rigid;
     private float hitTime;
     private float hitTimer;
     private bool canHit;
-    //private bool isRunning;
+    private bool isRunning;
+    private Animator anim;
+    private bool isDead;
+    private float deathTimer;
+    private float deathTime;
 
     void Start()
     {
         currHealth = maxHealth;
-        hitTime = 0.5f;
+        rigid = GetComponent<Rigidbody>();
+        hitTime = 0.4f;
         hitTimer = 0;
         canHit = true;
-
-        Debug.Log(currHealth);
+        isRunning = false;
+        anim = GetComponentInChildren<Animator>();
+        isDead = false;
+        deathTime = 5;
+        deathTimer = 0;
     }
 
     void Update()
     {
-        
+        if (!isPaused && !isDead)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift) && !isRunning)
+            {
+                speed *= 2;
+                isRunning = true;
+            }
+            if (Input.GetKeyUp(KeyCode.CapsLock) && isRunning)
+            {
+                speed /= 2;
+                isRunning = false;
+            }
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                anim.SetBool("IsAttacking",true);
+            }
+            else
+            {
+                anim.SetBool("IsAttacking", false);
+            }
+
+            float horizontal = Input.GetAxis("Horizontal") * speed;
+            float vertical = Input.GetAxis("Vertical") * speed;
+
+            Movement(horizontal, vertical);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (isGrounded)
+                {
+                    isGrounded = false;
+                    StartCoroutine(Jump());
+                }
+            }
+        }
+        if (deathTimer >= deathTime)
+        {
+            transform.position = respawn;
+            transform.rotation = respawnRotation;
+            currHealth = maxHealth;
+            //healthBar.value = maxHealth;
+
+            isDead = false;
+            deathTimer = 0;
+        }
     }
 
     void LateUpdate()
     {
         hitTimer += Time.deltaTime;
         if (hitTimer > hitTime) canHit = true;
+
+        if (isDead)
+        {
+            deathTimer += Time.deltaTime;
+        }
     }
-    
-    //Control cursor visibility on pause
-    //public void Paused(bool paused)
-    //{
-    //    isPaused = paused;
 
-    //    if(isPaused)
-    //    {
-    //        Cursor.visible = true;
-    //    }
-    //    else
-    //    {
-    //        Cursor.visible = false;
-    //    }
-    //}
+    IEnumerator Jump()
+    {
+        anim.SetBool("IsJumping", true);
 
-    //Take damage on touch enemy
-    //void OnCollisionEnter(Collision coll)
-    //{
-    //    rigid.angularVelocity = new Vector3(0, 0, 0);
-    //    if (coll.gameObject.CompareTag("Enemy"))
-    //    {
-    //        TakeDamage(coll.collider.gameObject.GetComponent<Enemy>().power);
-    //    }
-    //}
+        yield return new WaitForSeconds(0.5f);
+
+        rigid.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+    }
+
+    void Movement(float horizontal, float vertical)
+    {
+        if (horizontal != 0f || vertical != 0f)
+        {
+            Vector3 direction = Camera.main.transform.TransformDirection(new Vector3(horizontal, 0, vertical));
+            rigid.velocity = new Vector3(direction.x, rigid.velocity.y, direction.z);
+
+            transform.rotation = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0);
+
+            if (isGrounded)
+            {
+                anim.SetBool("IsWalking", true);
+                anim.SetFloat("Blend", horizontal);
+            }
+
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+    }
+
+    public void Paused(bool paused)
+    {
+        isPaused = paused;
+
+        if (isPaused)
+        {
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.visible = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        rigid.angularVelocity = new Vector3(0, 0, 0);
+        if (coll.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(coll.collider.gameObject.GetComponent<Enemy>().power);
+        }
+    }
 
     void TakeDamage(float damage)
     {
         if (!canHit) return;
 
+        if (isDead || isPaused) return;
+
         currHealth -= damage;
         //healthBar.value = currHealth;
-
-        Debug.Log(currHealth);
 
         hitTimer = 0;
         canHit = false;
 
         if (currHealth <= 0)
         {
-            transform.position = respawn;
-            currHealth = maxHealth;
-            //healthBar.value = maxHealth;
+            isDead = true;
+        }
+    }
+
+    void OnCollisionStay(Collision coll)
+    {
+        rigid.angularVelocity = new Vector3(0, 0, 0);
+
+        if (coll.gameObject.CompareTag("Wall"))
+        {
+            isGrounded = false;
+        }
+        else if(coll.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            anim.SetBool("IsJumping", false);
         }
     }
 
@@ -96,13 +196,14 @@ public class Player : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Enemy Weapon"))
         {
-            TakeDamage(other.gameObject.GetComponent<EnemyWeapon>().power);
+            GameObject enemy = other.gameObject.transform.parent.gameObject;
+            if (enemy.CompareTag("Enemy")) TakeDamage(other.gameObject.transform.parent.GetComponent<Enemy>().power);
 
-            //float force = 2;
-            //float height = 2;
-            //Vector3 enemyDir = other.gameObject.transform.forward;
-            //Vector3 dir = new Vector3(enemyDir.x * force, height, enemyDir.z * force);
-            //rigid.AddForce(dir, ForceMode.Impulse);
+            float force = 2;
+            float height = 2;
+            Vector3 enemyDir = other.gameObject.transform.forward;
+            Vector3 dir = new Vector3(enemyDir.x * force, height, enemyDir.z * force);
+            rigid.AddForce(dir, ForceMode.Impulse);
         }
     }
 }
